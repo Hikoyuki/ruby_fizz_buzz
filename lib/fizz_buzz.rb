1a99cf879b4861eb790c30de41e938edff67b682
酒井彦行 <hikoyuki.sakai@gmail.com>
2022-03-09 00:56:18 +0900

    rubocop対応

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,11 @@
 def fizz_buzz(number)
   n = number
   if n % 15 == 0
-   'Fizz Buzz'
+    'Fizz Buzz'
   elsif n % 3 == 0
-   'Fizz'
+    'Fizz'
   elsif n % 5 == 0
-   'Buzz'
+    'Buzz'
   else
     n.to_s
   end
